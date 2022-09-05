a75bce7302957460e471ae5cdbe7e2e6d5017972
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 10:18:27 +0400

    Задание 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,11 @@
-console.log(6);
+const getUsersWithAge = (users, min, max) => {
+  return users.filter((user) => {
+    if (user.age > min && user.age < max) {
+      return user;
+    }
+  });
+};
+
+console.log(getUsersWithAge(users, 20, 30));
+
+console.log(getUsersWithAge(users, 30, 40));
