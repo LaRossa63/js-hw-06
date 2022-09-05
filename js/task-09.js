3a3cc6c1d57773d0c4933ea661e6fbc16e220715
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 11:07:15 +0400

    Задание 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,1 +1,7 @@
-console.log(9);
+const getNamesSortedByFriendsCount = (users) => {
+  const objSort = users.sort((a, b) => a.friends.length - b.friends.length);
+
+  return objSort.map((user) => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
