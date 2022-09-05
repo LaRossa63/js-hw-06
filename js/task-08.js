7f343b2253233b07aa4a09e813e243e0c9299c8a
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 10:57:27 +0400

    Задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-console.log(8);
+const getUsersWithFriend = (users, friendName) => {
+  let result = [];
+
+  users.forEach((user) => {
+    if (user.friends.includes(friendName)) {
+      result.push(user.name);
+    }
+  });
+
+  return result;
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
