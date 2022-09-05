90936e8b80842c4026bbd97ba164054e04c0149a
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 10:15:09 +0400

    Задание 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,5 @@
-console.log(4);
+const getInactiveUsers = (users) => {
+  return users.filter((user) => user.isActive === false);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
